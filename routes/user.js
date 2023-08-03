57a09a7134c8e3051062745ee53dda279141c9bb
Taylor McKeel <josephmckeel1993@gmail.com>
2023-08-03 16:22:23 +0300

    added user routes

---
+++ routes/user.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+  getUsers,
+  createUser,
+  deleteUsers,
+  getUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController')
+
+router.route('/')
+  .get(getUsers)
+  .post(createUser)
+  .delete(deleteUsers)
+
+
+router.route('/:userId')
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser)
+
+
+module.exports = router
