dafa7f2c0519c4426f058ade87665d5952df83ae
Taylor McKeel <josephmckeel1993@gmail.com>
2024-09-07 17:18:18 -0400

    updated winston logger

--- controllers/utils/logger.js
+++ controllers/utils/logger.js
@@ -1,14 +1,28 @@
 const winston = require('winston');
-const { combine, timestamp, json } = winston.format;
+const { combine, timestamp, json, colorize, simple, printf } = winston.format;
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(
     timestamp(),
-    json()
+    json(),
+    colorize(),
+    simple(),
+    printf(({timestamp, level, message})=>{
+      return `${timestamp} ${level}: ${message}`
+    })
+
   ),
   transports: [
-    new winston.transports.Console()
+    new winston.transports.Console({
+      level: 'info',
+      handleExceptions: true,
+      format: combine(
+        json(),
+        colorize(),
+        simple(),
+      )
+    }),
   ],
 })
 
